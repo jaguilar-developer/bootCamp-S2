579b2b805ef2826e76849ee7217524acdfe269e4
jaguilar-developer <u21229611@utp.edu.pe>
2022-04-20 19:34:44 -0500

    Se agrega la funcionalidad checkstyle

--- customers/src/main/java/com/bootcamp/bank/customers/controller/OperationController.java
+++ customers/src/main/java/com/bootcamp/bank/customers/controller/OperationController.java
@@ -5,7 +5,14 @@ import com.bootcamp.bank.customers.service.OperationService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @RestController
